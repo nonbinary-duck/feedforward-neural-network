d2831fd8df4faddcc0de25b5cdc49585397ff2b1
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2022-04-18 17:35:44 +0100

    Working on the training

--- main.cpp
+++ main.cpp
@@ -13,20 +13,21 @@ using namespace ai_assignment;
 int main()
 {
     // Initalise a 'random' seed for std::rand
-    std::srand( std::time(0) );
+    std::srand( std::time(nullptr) );
     
-    const std::function<double(double)> &f = activation_functions::sigmoidFunc;
-
-    cout << f(0.9l) << endl;
-    cout << f(0.2l) << endl;
-    cout << f(0.0l) << endl;
-    cout << f(-0.1l) << endl;
-    cout << f(-0.9l) << endl;
-
     std::vector<double> weights { 0.5, -0.1, 0.2 };
     std::vector<double> inputs  { 0.0, 1.0, 1.0  };
 
     Neuron n(2, weights, activation_functions::sigmoidFunc);
+
+    cout << n.ProcessInputs(inputs) << endl << endl;
+
+    std::vector<TrainingExample> ex
+    {
+        TrainingExample({1.0, 0.0}, 1.0)
+    };
+
+    n.TrainNeuron(ex, 0.05);
 
     cout << n.ProcessInputs(inputs) << endl << endl;
 
